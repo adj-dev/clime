44f02bff50445a8561c6d6f5b3f8fd9289285e7a
Andrew Johnson <andrew.johnson612@gmail.com>
2019-01-12 13:36:22 -0600

    Changed app font / changed content of Header

--- src/components/DailyForecast.js
+++ src/components/DailyForecast.js
@@ -9,14 +9,17 @@ function DailyForecast({ forecast }) {
 
   return daily.data.map(data => {
     const timeStamp = moment.unix(data.time);
-    const day = timeStamp.format('dddd');
+    const dayOfWeek = timeStamp.format('ddd');
+    console.log(timeStamp);
 
     return (
       <div className="daily-row" key={data.time}>
-        <div className="day">{day}</div>
+        <div className="day">{dayOfWeek}</div>
         <i className={setIcon(data.icon)} />
-        <div>{Math.round(data.temperatureHigh)}</div>
-        <div>{Math.round(data.temperatureMin)}</div>
+        <div>
+          H {Math.round(data.temperatureHigh)} L{' '}
+          {Math.round(data.temperatureMin)}
+        </div>
       </div>
     );
   });
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header(props) {
   return (
     <div className="header">
-      <div className="title">Weather App</div>
+      <div className="title">Weather for Elk River, MN</div>
       <div className="credit">
         <a href="https://darksky.net/poweredby/">Powered by Dark Sky</a>
       </div>
